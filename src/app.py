db125f334959269aba87eca2f723022226be9824
Dongwooo Kim <kdw2003914@naver.com>
2023-05-12 13:17:28 +0900

    ADD comment about renameFile of App class

--- src/app.py
+++ src/app.py
@@ -279,12 +279,13 @@ class App(QMainWindow):  # main application window를 위한 클래스
             else:
                 self.mainModel.remove(file)
     def renameFile(self, event):#선택된 파일을 mainExplorer.selectionModel().selectedIndexes()로 받아와서 이름을 바꿔줌
-        selectedIndexes = self.mainExplorer.selectionModel().selectedIndexes();
-        for file in selectedIndexes:
-            fileName = self.mainModel.itemData(file)[0]
-            filePath = join(self.currentDir, fileName)
-            if os.path.exists(filePath):
-                self.mainExplorer.edit(file)
+        selectedIndexes = self.mainExplorer.selectionModel().selectedIndexes(); #선택된 파일들의 인덱스를 받아옴
+        for file in selectedIndexes: #선택된 파일들의 인덱스를 하나씩 돌면서
+            fileName = self.mainModel.itemData(file)[0] #선택된 파일의 이름을 받아옴
+            filePath = join(self.currentDir, fileName) #선택된 파일의 경로를 받아 옴
+            if os.path.exists(filePath): #선택된 파일이 존재한다면
+                self.mainExplorer.edit(file) #선택된 파일의 이름을 바꿔줌
+                self.mainExplorer.edit(file) #선택된 파일의 이름을 바꿔줌
 
     def createTopMenu(self):
         # Add menus
