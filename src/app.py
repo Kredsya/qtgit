485a648d7752f1a9554c7e63a74d37583e62fe87
kredsya <do901328@gmail.com>
2023-05-14 22:16:31 +0900

    Fix Prototype of GitCommit, need to test

--- src/app.py
+++ src/app.py
@@ -264,10 +264,11 @@ class App(QMainWindow):  # main application window를 위한 클래스
             print("git init을 먼저 하세요.")
             QMessageBox.warning(self, "Warning", "git init을 먼저 하세요.", QMessageBox.Ok)
     
+    # need to test
     def GitCommit(self):
         path = self.mainModel.filePath(self.mainExplorer.currentIndex())  # QFileSystemModel의 현재 디렉토리의 경로를 반환하는 함수
         path = path.rsplit('/', 1)[0]
-        if os.path.isdir(path + '/.git'):
+        if self.is_gitrepo(path):
             text, ok = QInputDialog.getText(self, 'Commit Message', 'Enter commit message')
             if ok:
                 os.system('git commit -m "' + text + '"')
