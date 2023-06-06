9fab78413ec2f555a2ddf2869e0b7b6c58eee6a1
kredsya <do901328@gmail.com>
2023-06-06 15:42:11 +0900

    Add _branchAction.py file and ready to implement

---
+++ src/_branchAction.py
@@ -0,0 +1,14 @@
+
+
+class branchAction():
+    def BranchCreate(self):
+        print("Hello")
+    
+    def BranchDelete(self):
+        print("hello")
+    
+    def BranchRename(self):
+        print("HeLLo")
+    
+    def BranchCheckout(self):
+        print("tmp")
