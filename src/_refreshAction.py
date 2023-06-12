ae5dbed7aa086ba748e83c4099f1819816292eeb
kredsya <do901328@gmail.com>
2023-06-12 21:39:51 +0900

    Fix : replace os.popen to subprocess.check_output

--- src/_refreshAction.py
+++ src/_refreshAction.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 from utility import parse_git_status, is_gitrepo, parse_git_current_branch
 from _eventController import eventController
 
@@ -10,7 +10,10 @@ class refreshAction(eventController):
         path = self.mainModel.setRootPath(self.currentDir)
         if is_gitrepo(self.currentDir):
             os.chdir(self.currentDir)
-            statuses_str = os.popen("git status").read()
+            try:
+                statuses_str = subprocess.check_output(['git', 'status'], shell=True, stderr=subprocess.STDOUT).decode('utf-8')
+            except Exception as e:
+                statuses_str = e.output.decode()
             git_statuses = parse_git_status(statuses_str)
             self.git_status_column_update(self.currentDir, git_statuses)
             self.currentBranch = parse_git_current_branch(statuses_str)
